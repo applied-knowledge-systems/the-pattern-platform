e34c789d014e99afa209e13eaa34db13a046223f
Alex Mikhalev <alex@sci-blog.com>
2021-04-13 20:44:08 +0100

    Moved Automata into separate repo and made it fetch over http (import). Let security people scream :)
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>

--- sentences_matcher_register.py
+++ sentences_matcher_register.py
@@ -15,42 +15,12 @@ def connecttoRedis():
     return redis_client
 
 
-def loadAutomata():
-    from urllib.request import urlopen
-    import ahocorasick 
-    import joblib
-    
-    try:
-        Automata=joblib.load("./automata/automata_fresh.pkl")
-    except:
-        Automata=joblib.load(urlopen("https://github.com/AlexMikhalev/cord19redisknowledgegraph/raw/master/automata/automata_syns.pkl.bz2"))
-    
-    log("Automata properties " + str(Automata.get_stats()))
-    return Automata
-
-def find_matches(sent_text, A):
-    matched_ents = []
-    for char_end, (eid, ent_text) in A.iter(sent_text):
-        char_start = char_end - len(ent_text)
-        matched_ents.append((eid, ent_text, char_start, char_end))
-    # remove shorter subsumed matches
-    longest_matched_ents = []
-    for matched_ent in sorted(matched_ents, key=lambda x: len(x[1]), reverse=True):
-        longest_match_exists = False
-        char_start, char_end = matched_ent[2], matched_ent[3]
-        for _, _, ref_start, ref_end in longest_matched_ents:
-            if ref_start <= char_start and ref_end >= char_end:
-                longest_match_exists = True
-                break
-        if not longest_match_exists:
-            # print("adding match to longest")
-            longest_matched_ents.append(matched_ent)
-    return [t for t in longest_matched_ents if len(t[1])>3] 
-
-
-
 def OnRegisteredAutomata():
     global Automata
+    import httpimport
+    with httpimport.remote_repo(['utils'], "https://raw.githubusercontent.com/applied-knowledge-systems/the-pattern-automata/main/automata/"):
+        import utils
+    from utils import loadAutomata
     Automata=loadAutomata()
     return Automata
 
@@ -61,6 +31,10 @@ def process_item(record):
     with httpimport.remote_repo(['stop_words'], "https://raw.githubusercontent.com/explosion/spaCy/master/spacy/lang/en/"):
         import stop_words
     from stop_words import STOP_WORDS
+    with httpimport.remote_repo(['utils'], "https://raw.githubusercontent.com/applied-knowledge-systems/the-pattern-automata/main/automata/"):
+        import utils
+    from utils import loadAutomata, find_matches
+
     from string import punctuation
     import itertools
     import re
